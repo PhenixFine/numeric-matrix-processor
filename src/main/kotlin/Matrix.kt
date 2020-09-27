b4bcaf36c1e013a927821ffe7e16aa84b287766b
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-09-26 20:34:27 -0400

    Matrix.kt - changes: • adjusted the formatting of add() and times(matrix). • added transposeMain(), transposeSide(), transposeVertical(), and transposeHorizontal() that all call added private transpose() to do the work, which returns the matrix transformed according to the number it receives ( 1 - main, 2 - side, 3 - vertical, and 4 - horizontal ).

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -3,9 +3,7 @@ data class Matrix(val column: List<Row>) {
     fun add(matrix: Matrix): Matrix? {
         return if (this.column.size == matrix.column.size && this.column[0].row.size == matrix.column[0].row.size) {
             val hold = mutableListOf<Row>()
-            for (i in this.column.indices) {
-                hold.add(this.column[i].add(matrix.column[i]))
-            }
+            for (i in this.column.indices) hold.add(this.column[i].add(matrix.column[i]))
             Matrix(hold)
         } else null
     }
@@ -21,12 +19,10 @@ data class Matrix(val column: List<Row>) {
             val holdMatrix = mutableListOf<Row>()
             for (i in matrix.column) {
                 val holdRow = mutableListOf<Double>()
-                for (j in this.column[0].row.indices) {
-                    holdRow.add(i.times(this.columnToRow(j)))
-                }
+                for (j in this.column[0].row.indices) holdRow.add(i.times(this.columnToRow(j)))
                 holdMatrix.add(Row(holdRow))
             }
-            return Matrix(holdMatrix)
+            Matrix(holdMatrix)
         } else null
     }
 
@@ -34,6 +30,25 @@ data class Matrix(val column: List<Row>) {
         val hold = mutableListOf<Double>()
         for (i in this.column.indices) hold.add(this.column[i].row[index])
         return Row(hold)
+    }
+
+    fun transposeMain() = transpose(1)
+
+    fun transposeSide() = transpose(2)
+
+    fun transposeVertical() = transpose(3)
+
+    fun transposeHorizontal() = transpose(4)
+
+    private fun transpose(type: Int): Matrix {
+        val holdMatrix = mutableListOf<Row>()
+        when (type) {
+            1, 3 -> for (i in this.column.indices)
+                holdMatrix.add(if (type == 3) this.column[i].reversed() else columnToRow(i))
+            else -> for (i in this.column.lastIndex downTo 0)
+                holdMatrix.add(if (type == 2) columnToRow(i).reversed() else this.column[i])
+        }
+        return Matrix(holdMatrix)
     }
 
     fun print() {
